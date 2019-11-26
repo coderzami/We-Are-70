47382f373d5a5983c4650bb68556c097021de002
Zamiur Rahman <zamiur53@gmail.com>
2019-11-26 22:31:40 +0600

    Display message on the screen

--- app/src/main/java/com/teambrj/weare70/ChatActivity.java
+++ app/src/main/java/com/teambrj/weare70/ChatActivity.java
@@ -1,5 +1,7 @@
 package com.teambrj.weare70;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -8,8 +10,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.teambrj.weare70.Chat.MessageAdapter;
@@ -28,6 +34,9 @@ public class ChatActivity extends AppCompatActivity {
     ArrayList<MessageObject> messageList;
 
     String chatID;
+    DatabaseReference mChatDb;
+
+    private EditText mMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +44,9 @@ public class ChatActivity extends AppCompatActivity {
         setContentView(R.layout.activity_chat);
 
         chatID = getIntent().getExtras().getString("chatID");
+        mChatDb =  FirebaseDatabase.getInstance().getReference().child("chat").child(chatID);
+
+        mMessage = findViewById(R.id.message);
 
         Button mSend = findViewById(R.id.send);
         mSend.setOnClickListener(new View.OnClickListener() {
@@ -45,15 +57,55 @@ public class ChatActivity extends AppCompatActivity {
         });
 
         initializeRecyclerView();
+        getChatMessages();
+    }
+
+    private void getChatMessages() {
+        mChatDb.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                if(dataSnapshot.exists()){
+                    String  text= "",
+                            creatorID="";
+                    if(dataSnapshot.child("text").getValue() != null)
+                        text = dataSnapshot.child("text").getValue().toString();
+                    if(dataSnapshot.child("creator").getValue() != null)
+                        creatorID = dataSnapshot.child("creator").getValue().toString();
+
+                    MessageObject mMessage = new MessageObject(dataSnapshot.getKey(),creatorID,text);
+                    messageList.add(mMessage);
+                    mChatAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     private void sendMessage() {
-        EditText mMessage = findViewById(R.id.message);
 
         if(!mMessage.getText().toString().isEmpty()){
-            DatabaseReference newMessageDB = FirebaseDatabase.getInstance().getReference().child("chat").child(chatID).push();
+            DatabaseReference newMessageDB = mChatDb.push();
 
-            Map newMessageMap = new HashMap();
+            Map newMessageMap = new HashMap<>();
             newMessageMap.put("text",mMessage.getText().toString());
             newMessageMap.put("creator", FirebaseAuth.getInstance().getUid());
 
